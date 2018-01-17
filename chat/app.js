c3e978226200ba43118b18a3b1b5f281fe1d071c
sunnyharsh <sunnyharshsunny12345@gmail.com>
2018-01-17 18:07:02 +0530

    chat through ngrok
    chat byngrok

---
+++ chat/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var socket = require("socket.io");
+var app  = express();
+
+
+app.use(express.static("public"));
+var server  = app.listen(9000,function(){
+	console.log("Server Start....");
+})
+var io = socket(server);
+io.sockets.on('connection', function (socket) { //one time run called hand shaking
+	console.log('connection :', socket.request.connection._peername);
+	socket.emit('message', { message: 'Server is Saying welcome to the chat'+socket.id });
+		
+	
+	socket.on('send', function (data) {
+		io.sockets.emit('message', data);
+    });
+});
